1b7432851e0023877e68589b8144cf39be8ad615
Kraus Milan <milan.kraus@vacuumlabs.com>
2019-07-14 23:53:09 +0200

    Implemented faster LRU cache

--- src/lruCache.js
+++ src/lruCache.js
@@ -1,46 +1,97 @@
 // @flow
-import type { Entry, Equals } from ".";
+import type { Equals } from ".";
 
-const findIndex = (arr: any[], fn: any => any) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i])) {
-      return i;
+export default function lruCache(capacity: number, equals: Equals) {
+  var _keys = new Array(capacity);
+  var _values = new Array(capacity);
+
+  var _next = new Array(capacity);
+  var _previous = new Array(capacity);
+
+  var _head = 0;
+  var _tail = 0;
+
+  var _size = 0;
+
+  function findIndex(key: any) {
+    if (_size === 0) {
+      return undefined;
     }
-  }
 
-  return -1;
-};
+    var index = _head;
 
-export default function lruCache(limit: number, equals: Equals) {
-  const entries: Entry[] = [];
-
-  function get(key: any) {
-    const cacheIndex = findIndex(entries, entry => equals(key, entry.key));
-
-    // We found a cached entry
-    if (cacheIndex > -1) {
-      const entry = entries[cacheIndex];
-
-      // Cached entry not at top of cache, move it to the top
-      if (cacheIndex > 0) {
-        entries.splice(cacheIndex, 1);
-        entries.unshift(entry);
+    while (index !== undefined) {
+      if (equals(key, _keys[index])) {
+        return index;
       }
 
-      return entry.value;
+      if (index === _tail) {
+        break;
+      }
+
+      index = _next[index];
     }
 
-    // No entry found in cache, return null
     return undefined;
   }
 
-  function put(key: any, value: any) {
-    if (!get(key)) {
-      entries.unshift({ key, value });
-      if (entries.length > limit) {
-        entries.pop();
-      }
+  function addOnTop(index: number) {
+    if (_head === index) {
+      return;
     }
+
+    var previous = _previous[index];
+    var next = _next[index];
+
+    if (_tail === index) {
+      _tail = previous;
+    } else {
+      _previous[next] = previous;
+    }
+
+    _next[previous] = next;
+
+    var oldHead = _head;
+    _previous[oldHead] = index;
+    _head = index;
+    _next[index] = oldHead;
+  }
+
+  function put(key: any, value: any) {
+    var index = findIndex(key);
+
+    if (index !== undefined) {
+      addOnTop(index);
+      _values[index] = value;
+
+      return;
+    }
+
+    if (_size < capacity) {
+      index = _size++;
+    } else {
+      index = _tail;
+      _tail = _previous[index];
+    }
+
+    _keys[index] = key;
+    _values[index] = value;
+
+    _next[index] = _head;
+    _previous[_head] = index;
+    _head = index;
+  }
+
+  function get(key: any) {
+    var index = findIndex(key);
+
+    if (index === undefined) {
+      return;
+    }
+
+    addOnTop(index);
+
+    return _values[index];
   }
 
   return { get, put };
