46ce92d473a7e71c746671bb85829c3b5b801b4e
Ivan Lagunovsky <ivan.lagunovsky@gmail.com>
2019-09-11 13:14:41 +0300

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,9 @@
-declare const memoize = (
-  limit?: number,
-  equals?: (a: any, b: any) => boolean,
-  deepObjects?: boolean
-) => <T extends Function>(func: T) => T;
-export default memoize;
+declare module 'lru-memoize' {
+  function memoize(
+    limit?: number,
+    equals?: (a: any, b: any) => boolean,
+    deepObjects?: boolean,
+  ): <T extends Function>(func: T) => T;
+
+  export default memoize;
+}
